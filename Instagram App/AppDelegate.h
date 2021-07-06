2268bbf76e588df4ac986c5d46757deff419ed42
ileenf <ifan2011@gmail.com>
2021-07-06 13:59:27 -0700

    Initial Commit

---
+++ Instagram App/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram App
+//
+//  Created by Ileen Fan on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Instagram App/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Instagram App
+//
+//  Created by Ileen Fan on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
