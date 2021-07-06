8386faeb419f331f1318d45004db0cd84e3169c2
ileenf <ifan2011@gmail.com>
2021-07-06 14:50:12 -0700

    Add image icons to launch screen/app icon and login/sign up functionality

--- Instagram App/AppDelegate.m
+++ Instagram App/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,27 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+            configuration.applicationId = @"0VoO5VzzRd5PyX4f4PIfyXDDV1cyaO019xnkU9wa"; // <- UPDATE
+            configuration.clientKey = @"cPokj0sbWmiH4WHRaGody5F99O9NSPTke9sCgzJa"; // <- UPDATE
+            configuration.server = @"https://parseapi.back4app.com";
+        }];
+
+        [Parse initializeWithConfiguration:config];
+    
+    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
+     gameScore[@"score"] = @1337;
+     gameScore[@"playerName"] = @"Sean Plott";
+     gameScore[@"cheatMode"] = @NO;
+     [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+      if (succeeded) {
+             NSLog(@"Object saved!");
+      } else {
+             NSLog(@"Error: %@", error.description);
+      }
+     }];
+    
     return YES;
 }
 
---
+++ Instagram App/LoginViewController.m
@@ -0,0 +1,133 @@
+//
+//  LoginViewController.m
+//  Instagram App
+//
+//  Created by Ileen Fan on 7/6/21.
+//
+
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)didLogin:(id)sender {
+    
+    NSString *username = self.usernameField.text;
+        NSString *password = self.passwordField.text;
+    
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Login Failed"
+                                                                                   message:@"Bad or missing username/password"
+                                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *TryAgainAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                            style:UIAlertActionStyleCancel
+                                                          handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle cancel response here. Doing nothing will dismiss the view.
+                                                          }];
+        // add the cancel action to the alertController
+        [alert addAction:TryAgainAction];
+        
+        [self presentViewController:alert animated:YES completion:^{
+            // optional code for what happens after the alert controller has finished presenting
+        }];
+    }
+        
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Login Failed"
+                                                                                           message:@"Invalid username/password"
+                                                                                    preferredStyle:(UIAlertControllerStyleAlert)];
+                UIAlertAction *TryAgainAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                                    style:UIAlertActionStyleCancel
+                                                                  handler:^(UIAlertAction * _Nonnull action) {
+                                                                         // handle cancel response here. Doing nothing will dismiss the view.
+                                                                  }];
+                // add the cancel action to the alertController
+                [alert addAction:TryAgainAction];
+                
+                [self presentViewController:alert animated:YES completion:^{
+                    // optional code for what happens after the alert controller has finished presenting
+                }];
+            } else {
+                NSLog(@"User logged in successfully");
+                [self performSegueWithIdentifier:@"LoginSegue" sender:nil];
+                
+                // display view controller that needs to shown after successful login
+            }
+        }];
+}
+
+- (IBAction)didSignup:(id)sender {
+    PFUser *newUser = [PFUser user];
+        
+        // set user properties
+        newUser.username = self.usernameField.text;
+        newUser.password = self.passwordField.text;
+    
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Sign up Failed"
+                                                                                   message:@"Bad or missing username/password"
+                                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+        UIAlertAction *TryAgainAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                            style:UIAlertActionStyleCancel
+                                                          handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle cancel response here. Doing nothing will dismiss the view.
+                                                          }];
+        // add the cancel action to the alertController
+        [alert addAction:TryAgainAction];
+        
+        [self presentViewController:alert animated:YES completion:^{
+            // optional code for what happens after the alert controller has finished presenting
+        }];
+    }
+        
+        // call sign up function on the object
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Sign up Failed"
+                                                                                           message:@"Already existing username/password"
+                                                                                    preferredStyle:(UIAlertControllerStyleAlert)];
+                UIAlertAction *TryAgainAction = [UIAlertAction actionWithTitle:@"Try Again"
+                                                                    style:UIAlertActionStyleCancel
+                                                                  handler:^(UIAlertAction * _Nonnull action) {
+                                                                         // handle cancel response here. Doing nothing will dismiss the view.
+                                                                  }];
+                // add the cancel action to the alertController
+                [alert addAction:TryAgainAction];
+                
+                [self presentViewController:alert animated:YES completion:^{
+                    // optional code for what happens after the alert controller has finished presenting
+                }];
+                
+            } else {
+                NSLog(@"User registered successfully");
+                
+                // manually segue to logged in view
+            }
+        }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
